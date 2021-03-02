dad9593f39fd1dfc85cb0ad9bcb59c4855e27731
sleepyman212 <sleepyman212@gmail.com>
2021-03-02 22:36:22 +0800

    init front end

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import 'reset-css';
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
