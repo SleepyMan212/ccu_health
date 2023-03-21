f9b37a24170cc36166d4f80feadc679b6cfc0481
sleepyman212 <sleepyman212@gmail.com>
2023-03-21 23:24:46 +0800

    add new api on users

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,16 +4,69 @@ require('dotenv').config();
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 const db = require('../models');
-const { Users } = db;
+const { Op } = require('sequelize');
+const { sequelize, Users } = db;
+const admin = require('./middleware/admin');
+const auth = require('./middleware/auth');
 
 const saltRounds = 10;
-// const myPassword = 'password1';
-// const testPassword = 'password2';
 
-/* GET users listing. */
-// router.get('/', function(req, res) {
-//   res.send('respond with a resource');
-// });
+router.get('/', auth, admin, async function (req, res) {
+  const users = await Users.findAll({
+    attributes: { 
+      exclude: ['password'],
+      order: [['id', 'DESC']]
+    },
+    where: {
+      auth: {
+        [Op.ne]: 1 // 1 is for admin, 2 is for manager
+      }
+    }
+  });
+  res.status(200).json(users);
+});
+
+router.delete('/:id',auth, admin, async function (req, res) {
+  const t = await sequelize.transaction();
+  try {
+    const { id } = req.params;
+    const data = await Users.destroy({
+      where: {
+        id
+      }
+    }, { transaction: t });
+    await t.commit();
+    res.json({ data }, 204);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ msg: "bad request" });
+    t.rollback();
+  }
+});
+
+router.put('/:id', auth, admin, async function (req, res) {
+  const t = await sequelize.transaction();
+  try {
+    const { id } = req.params;
+    const user = await Users.findByPk(id);
+    if (!user) res.status(200).json({msg: "No exist user"})
+    const { body } = req;
+    if (bcrypt.compareSync(user.password, body.oldPassword) && body.password === body.passwordAgain) {
+      const hash = bcrypt.hashSync(body.password, saltRounds);
+      await Users.update({
+        username: body.username,
+        auth: body.auth,
+        password: hash
+      },{ transaction: t});      
+    }
+    await t.commit();
+    res.status(204).json();
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ msg: "bad request" });
+    t.rollback();
+  }
+});
 
 router.post('/login', async function(req, res) {
   const { username, password } = req.body
@@ -24,7 +77,7 @@ router.post('/login', async function(req, res) {
   })
 
   if(bcrypt.compareSync(password, user.password)) {
-    const token = jwt.sign({ username, isAdmin:user.auth===1 }, process.env["JWT_SECRET"],{
+    const token = jwt.sign({ username, isAdmin: user.auth === 1, isManager: (user.auth === 1 || user.auth === 2) }, process.env["JWT_SECRET"],{
       expiresIn: process.env["EXPIRES_IN"]
     });
 
@@ -51,6 +104,13 @@ router.post('/login', async function(req, res) {
 
 router.get('/login', async function (req, res) {
   const { authorization } = req.headers;
+  console.log(authorization)
+  if(typeof authorization === "undefined") {
+    res.status(200).json({
+      status: false,
+      user: null
+    });
+  }
   const token = authorization.split(' ')[1];
   jwt.verify(token, process.env["JWT_SECRET"], (err, decode) => {
     if (err) {
@@ -68,14 +128,16 @@ router.get('/login', async function (req, res) {
 })
 
 
-router.post('/register', async function(req, res) {
-  const { username, password, passwordAgain } = req.body
+router.post('/register', auth, admin, async function(req, res) {
+  const { username, password, passwordAgain, auth } = req.body
+  console.log(req.body)
   try {
-    if (password === passwordAgain) {
+    if (password === passwordAgain && auth !== 1) {
       const hash = bcrypt.hashSync(password, saltRounds);
       const data = await Users.create({
         username,
         password: hash,
+        auth
       });
       res.status(201).json(data);
     } else {
